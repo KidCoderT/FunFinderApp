8419fe891f3de5aaa85c1db67d7558e13158917d
Tejas <Tejas75O25@gmail.com>
2021-09-17 10:02:11 +0530

    Added databinding and added authentication firebase ui
    there is styling probkem with authentication though

--- app/src/main/java/com/udacity/funfinder/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/funfinder/authentication/AuthenticationActivity.kt
@@ -2,11 +2,62 @@ package com.udacity.funfinder.authentication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.udacity.funfinder.R
+import com.udacity.funfinder.databinding.ActivityAuthenticationBinding
 
 class AuthenticationActivity : AppCompatActivity() {
+    // registers a callback for the FirebaseUI Activity result contract:
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+
+    companion object {
+        const val TAG = "AuthenticationActivity"
+    }
+
+    private lateinit var binding: ActivityAuthenticationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
+
+        binding = DataBindingUtil.setContentView(
+            this, R.layout.activity_authentication)
+
+        // Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.smili_icon_large)
+            .setTheme(R.style.firebase_ui_auth_style)
+            .build()
+
+        // Start the signup intent when clicked login button
+        binding.loginButton.setOnClickListener {
+            signInLauncher.launch(signInIntent)
+        }
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == RESULT_OK) {
+            Log.i(TAG, "It worked!")
+//            val intent = Intent(this, RemindersActivity::class.java)
+//            startActivity(intent)
+        } else {
+            Log.i(TAG, "Error")
+        }
     }
 }
