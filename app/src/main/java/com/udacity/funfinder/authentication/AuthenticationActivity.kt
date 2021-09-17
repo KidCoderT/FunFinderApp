cd359c24373512842e6221cb09634464ca0c7aa3
Tejas <Tejas75O25@gmail.com>
2021-09-17 07:07:45 +0530

    Added Authentication Page and services to app

---
+++ app/src/main/java/com/udacity/funfinder/authentication/AuthenticationActivity.kt
@@ -0,0 +1,12 @@
+package com.udacity.funfinder.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.udacity.funfinder.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
